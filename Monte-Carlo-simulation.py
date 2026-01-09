779d80562092ecb55515ebd52665859a4945753b
Finn04tools <duetschfi101369@th-nuernberg.de>
2026-01-09 22:33:54 +0100

    Update Monte-Carlo-simulation.py
    adding final docstrings

--- Monte-Carlo-simulation.py
+++ Monte-Carlo-simulation.py
@@ -43,16 +43,33 @@ def monte_carlo_simulation(simulation_seed):
 """
 Initializes the random number generator with the provided seed to ensure reproducibility.
 Repeats the roulette simulation for a predefined number of seeds (simulations).
+return gives the estimated probability that the player loses  at least 100 € 
+after 200 rounds in the Monte-Carlo simulation.
+The roulette simulation starts by opening a loop for the number of simulations of the first seed 
+setting the capital to 0. 
+it continues by opening another loop for the 200 rounds which using the propability (p_gewinn) 
+is adding 5€ or subtracting 5€ 
+After the 200 rounds if the capital is smaller or equals -100€ 1 is added to minus_100 and the second simulation
+starts. After all the simulations minus_100 is divided by the total number of simulations returning the 
+propability for the first seed. As the last step the first loop starts again with the second seed.
+this continues until all 10 seeds have a propability
 """
 random.seed(10)
 seeds = [random.randint(1, 1000) for _ in range(10)]
-
+"""
+creates 10 random seeds between 1 and 1000  
+"""
 print("Generated seeds:")
 print(seeds)
 print("\nSimulation results:\n")
-
+"""
+first prints generated seeds, then all seeds which were generated in a row and last it prints simulation results.
+"""
 for s in seeds:
     probability = monte_carlo_simulation(s)
     print(f"Seed {s}: {probability:.4%}")
+"""
+prints the first the generated seed and then the propability to lose 100€ for this seed
+"""
 
 
